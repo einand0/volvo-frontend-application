13754be0236ba2618c998e62016dfde63a12b0a1
Fernando Lopes <fernando._lopes@outlook.com>
2023-10-03 19:28:50 -0300

    feat: create card component

---
+++ src/components/CarCard.tsx
@@ -0,0 +1,37 @@
+import { Card, Spacer, CardContent, Text, Flex, Row, Link } from "vcc-ui"
+import { Car } from "../types/car.interface"
+import Image from "next/image"
+
+import styles from '../../public/css/CarCard.module.css'
+
+interface CardProps {
+    car: Car
+}
+
+export default function CarCard({ car }: CardProps) {
+    return (
+        <div className={styles.cardWrapper}>
+                <Text variant="bates" subStyle="emphasis">{car?.bodyType}</Text>
+                <Flex extend={{
+                    justifyContent: 'flex-start',
+                    alignItems: 'center',
+                    flexDirection: 'row',
+                    margin: 0}}
+                >
+                        <Text variant="amundsen">{car?.modelName}</Text>
+                        <Text variant="bates" subStyle="inline-link">{car?.modelType}</Text>
+                </Flex>
+                <Spacer />
+                <Image src={car.imageUrl} alt={car?.modelName} width="250" height="200" />
+                <Spacer />
+                <Flex extend={{
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                    flexDirection: 'row'
+                }}>
+                        <Link href="https://www.volvocars.com/" arrow="right">SHOP</Link>
+                        <Link href="https://www.volvocars.com/" arrow="right">LEARN</Link>
+                </Flex>
+        </div>
+    )
+}
